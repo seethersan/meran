5562849dd4b1a664f84b701bc120b84847495803
mpagano <mpagano@cespi.unlp.edu.ar>
2015-09-08 11:28:06 -0300

    GPLv3

--- docs/generacionInstalador/aplicar_licencia.sh
+++ docs/generacionInstalador/aplicar_licencia.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-for i in $(find -name *.pl); do
+
+cd /usr/share/meran/dev/
+
+for i in $(find -name '*.pl'); do
 	echo "#!/usr/bin/perl" > /tmp/auxiliar
-	LANG=es_ES.iso88591 cat ../licencia.txt >> /tmp/auxiliar
+	LANG=es_ES.iso88591 cat  /usr/share/meran/dev/docs/generacionInstalador/licencia.txt >> /tmp/auxiliar
 	LANG=es_ES.iso88591 sed s/^#.*//g $i >> /tmp/auxiliar
 	LANG=es_ES.iso88591 sed '/^$/d' /tmp/auxiliar > $i
 	chmod +x $i
 done;
-for i in $(find -name *.pm); do
-	LANG=es_ES.iso88591 cat ../licencia.txt > /tmp/auxiliar
+
+cd /usr/share/meran/dev/intranet/modules/C4/AR/
+
+for i in $(find -name '*.pm'); do
+	LANG=es_ES.iso88591 cat /usr/share/meran/dev/docs/generacionInstalador/licencia.txt > /tmp/auxiliar
 	LANG=es_ES.iso88591 sed s/^#.*//g $i >> /tmp/auxiliar
 	LANG=es_ES.iso88591 sed '/^$/d' /tmp/auxiliar > $i
 done;
